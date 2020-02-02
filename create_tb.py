a9ed513ffc373793f783c8d1226f68445e429682
ABHIJEETBISHT <akkinitj@gmail.com>
2020-02-02 21:45:05 +0530

    Added steps

--- create_tb.py
+++ create_tb.py
@@ -3,3 +3,13 @@ import os
 
 if(os.path.exists("mux.v")):
     print("Found !")
+
+#Script Steps
+# 1. Get top module
+# 2. Get IO ports
+# 3. Detect Clock and Reset(if any)
+# 4. Generate Clock and Reset sequence
+# 5. Toggle inputs across the Simulation time
+# 6. Observe (and monitor) output
+# 7. Keep a "final" kind of block to print Simulation summary
+# 8. Add a initial block to dump fsdb/vcd(waves etc)
